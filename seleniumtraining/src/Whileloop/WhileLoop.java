289269c1cecfdcd336bb1bb80eb138ceed7575ba
Admin <Admin@DESKTOP-RIH9D3K>
2023-07-22 12:25:59 +0530

    WhileLoop Example

---
+++ seleniumtraining/src/Whileloop/WhileLoop.java
@@ -0,0 +1,17 @@
+package Whileloop;
+
+public class WhileLoop {
+	public static void main(String args[])
+    {
+        // initialization expression
+        int i = 1;
+ 
+        // condition expression
+        while (i < 6) {
+            System.out.println("Hello World");
+ 
+            // update expression
+            i++;
+        }
+}
+}
