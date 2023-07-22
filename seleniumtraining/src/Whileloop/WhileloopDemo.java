917282907c424781c4429084fdfa65059181adeb
Admin <Admin@DESKTOP-RIH9D3K>
2023-07-22 12:17:24 +0530

    while do-while for Loop

---
+++ seleniumtraining/src/Whileloop/WhileloopDemo.java
@@ -0,0 +1,37 @@
+package Whileloop;
+
+class whileLoopDemo {
+    public static void main(String args[])
+    {
+        // initialization expression
+        int i = 1;
+ 
+        // condition expression
+        while (i < 6) {
+            System.out.println("Hello World");
+ 
+            // update expression
+            i++;
+        }
+        
+        //do while Loop
+     //   int i = 10;
+       // do {		System.out.println("Hello");
+        //			i++;
+        	//	}while(i < 5);
+        			
+       // for Loop  
+        //int i = 10;
+        //for ( int m = 5 , n = 0 ; m < 10 && n < 10 ; ++m, n++ ) {
+			// System.out.println("Hello"+m);
+	// }	
+    //System.out.println("it is out from loop");
+			
+			 // infinite
+		// for(;;) {
+			 
+		// }
+    }
+}
+
+
